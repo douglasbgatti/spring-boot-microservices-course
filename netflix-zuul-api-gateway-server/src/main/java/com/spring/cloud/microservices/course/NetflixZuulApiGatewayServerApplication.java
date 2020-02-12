34d29ea10342744bfc77f531c6cc3df03a01f997
Douglas gATTI <douglasbgatti@gmail.com>
2020-02-12 11:28:12 -0300

    Creating api gateway project

---
+++ netflix-zuul-api-gateway-server/src/main/java/com/spring/cloud/microservices/course/NetflixZuulApiGatewayServerApplication.java
@@ -0,0 +1,26 @@
+package com.spring.cloud.microservices.course;
+
+import brave.sampler.Sampler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.sleuth.instrument.messaging.ConsumerSampler;
+import org.springframework.context.annotation.Bean;
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class NetflixZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
+	}
+
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+
+}
